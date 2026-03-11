d7ccc636c2972b80463e9b77c605eb1d6b5ab95d
KUMARASWAMY S <87463693+kumaraswamys28@users.noreply.github.com>
2026-03-11 11:43:56 +0530

    Update model titles and URLs in modelService.js

--- services/modelService.js
+++ services/modelService.js
@@ -1,8 +1,7 @@
-
 const modelsData = [
   {
     id: 1,
-    title:"Hampi chariot",
+    title:"Hampi Stone Chariot",
     url: "/model.glb",
     transform: { position: [0, -1, 0], rotation: [0, 180, 0], scale: 1.5 },
     iot_data:
@@ -31,8 +30,8 @@ const modelsData = [
     {
     id: 2,
 
-   url: "/temple.glb",
-    title:"Pillar",
+   url: "/polee.glb",
+    title:"Stamba",
     transform: { position: [0, 0, 0], rotation: [500, 60, 40], scale: 1.5 },
     iot_data:
       "https://ykdqxxsnhfxlncttgxrk.supabase.co/rest/v1/iot_data?order=timestamp.desc&limit=1",
@@ -85,37 +84,11 @@ const modelsData = [
         //   url: "#",
         // },
       ],
-  },{
-    id: 4,
-    url: "/lib.glb",
-        // url: "/polee.glb",
-
-    title:"Library",
-    transform: { position: [0, 0, 0], rotation: [0, 180, 0], scale: 1.5 },
-    iot_data:
-      "https://ykdqxxsnhfxlncttgxrk.supabase.co/rest/v1/iot_data?order=timestamp.desc&limit=1",
-    api_key:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlrZHF4eHNuaGZ4bG5jdHRneHJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4MjEyNjUsImV4cCI6MjA4NzM5NzI2NX0.68SqWEPDhLwvrWcpMLKklWsA96mLq2Bpw55QUb55zRE",
-      items: [
-        {
-          title: "Dashboard",
-          url: "#",
-        },
-      {
-          title: "Analytics",
-          url: "#",
-        },
-        // {
-        //   title: "Billing22",
-        //   url: "#",
-        // },
-        // {
-        //   title: "Limits22",
-        //   url: "#",
-        // },
-      ],
-  },
+  }
 ];
+
+
+
 
 const fetchAllModels = () => modelsData.map(({ title, id, items }) => ({ title, id , items}));
 const fetchModelById = (id) => {
